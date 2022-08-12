03794876930fc14d132082ec2a5887aaaf33db1b
DEVL12 <Josediaz17822@gmail.com>
2022-08-12 19:43:07 -0400

    Adding buttons to the Pokédex and improving other things in the CSS

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BodyPokedex } from "../BodyPokedex/BodyPokedex";
 import { SearchPokemon } from "../SearchPokemon/SearchPokemon";
 import { ResultPokemon } from "../ResultPokemon/ResultPokemon";
+import { ButtonPokedex } from "../ButtonsPokedex/ButtonsPokedex";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <SearchPokemon />
       <BodyPokedex > 
         <ResultPokemon />
+        <ButtonPokedex />
       </BodyPokedex>
     </React.Fragment>
   );
---
+++ src/components/ButtonsPokedex/ButtonsPokedex.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './ButtonsPokedex.css';
+
+const ButtonPokedex = () => {
+  const mensaje = (msg) => {
+    alert(msg);
+  }
+
+  return ( // Luego los agregare con un ciclo
+    <>
+      <button className="btn-pokedex btn-lateral next-pokemon" type="button" onClick={() => mensaje('flecha derecha')}></button>
+      <button className="btn-pokedex btn-lateral previous-pokemon" type="button" onClick={() => mensaje('flecha izquierda')}></button>
+      <button className="btn-pokedex btn-vertical next-evolution-pokemon" type="button" onClick={() => mensaje('flecha arriba')}></button>
+      <button className="btn-pokedex btn-vertical previous-evolution-pokemon" type="button" onClick={() => mensaje('flecha abajo')}></button>
+      <button className="btn-pokedex add-favorite" type="button" onClick={() => mensaje('Boton circular')}></button>
+    </>
+  );
+};
+
+export { ButtonPokedex };
