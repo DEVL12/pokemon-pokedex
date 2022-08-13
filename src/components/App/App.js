87e9154555bd3383f6f6cffd6c9397ac03a07566
DEVL12 <Josediaz17822@gmail.com>
2022-08-13 11:46:13 -0400

    Improving the buttons

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,17 +3,23 @@ import { BodyPokedex } from "../BodyPokedex/BodyPokedex";
 import { SearchPokemon } from "../SearchPokemon/SearchPokemon";
 import { ResultPokemon } from "../ResultPokemon/ResultPokemon";
 import { ButtonPokedex } from "../ButtonsPokedex/ButtonsPokedex";
+import PokedexButtons from "./../../helpers/ButtonsPokedex.json";
 
 const App = () => {
   return (
     <React.Fragment>
       <SearchPokemon />
-      <BodyPokedex > 
+      <BodyPokedex>
         <ResultPokemon />
-        <ButtonPokedex />
+        {PokedexButtons.map((button_data) => (
+          <ButtonPokedex
+            key={button_data["class-name-button"]}
+            NameClass={button_data["class-name-button"]}
+          />
+        ))}
       </BodyPokedex>
     </React.Fragment>
   );
-}
+};
 
 export default App;
--- src/components/ButtonsPokedex/ButtonsPokedex.js
+++ src/components/ButtonsPokedex/ButtonsPokedex.js
@@ -1,20 +1,13 @@
 import React from "react";
 import './ButtonsPokedex.css';
 
-const ButtonPokedex = () => {
+const ButtonPokedex = ({NameClass}) => {
   const mensaje = (msg) => {
     alert(msg);
   }
 
-  return ( // Luego los agregare con un ciclo
-    <>
-      <button className="btn-pokedex btn-lateral next-pokemon" type="button" onClick={() => mensaje('flecha derecha')}></button>
-      <button className="btn-pokedex btn-lateral previous-pokemon" type="button" onClick={() => mensaje('flecha izquierda')}></button>
-      <button className="btn-pokedex btn-vertical next-evolution-pokemon" type="button" onClick={() => mensaje('flecha arriba')}></button>
-      <button className="btn-pokedex btn-vertical previous-evolution-pokemon" type="button" onClick={() => mensaje('flecha abajo')}></button>
-      <button className="btn-pokedex add-favorite" type="button" onClick={() => mensaje('Boton circular')}></button>
-      <button className="see-favorites" type="button" onClick={() => mensaje('Boton favoritos')}></button>
-    </>
+  return (
+    <button className={ NameClass } type="button" onClick={() => mensaje('Funciono')}></button>
   );
 };
 
