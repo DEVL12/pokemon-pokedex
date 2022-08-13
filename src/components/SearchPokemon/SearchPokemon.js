894dc4d920edc2cad37a1e18ad787315bf6c7e0c
DEVL12 <Josediaz17822@gmail.com>
2022-08-13 14:53:24 -0400

    Text input (Search pokemon) + JSON with all Pokemons

--- src/components/SearchPokemon/SearchPokemon.js
+++ src/components/SearchPokemon/SearchPokemon.js
@@ -1,11 +1,30 @@
 import React from "react";
 import "./SearchPokemon.css";
+import NamePokemon from './../../helpers/NamePokemon.json';
 
 const SearchPokemon = () => {
+  const [searchValue, setSearchValue] = React.useState([]);
+
+  const FindPokemonMatches = (e) => {
+    const Matchs = NamePokemon.filter(data => {
+      const valueInput = e.target.value.toLowerCase();
+      const Pokemon = data.name.toLowerCase();
+      return Pokemon.includes(valueInput);
+    });
+
+    setSearchValue(Matchs);
+    console.log(searchValue);
+  };
+
   return (
     <form className="form-pokemon" autoComplete="off">
-      <input className="search-pokemon search-success" type="text" placeholder="Nombre o numero del pokemon"></input>
-      <button className="search-button" type="button"> Buscar </button>
+      <input
+        className="search-pokemon search-success"
+        type="text"
+        placeholder="Nombre o numero del pokemon"
+        onChange={FindPokemonMatches}
+      />
+      <button className="search-button" type="button">Buscar</button>
     </form>
   );
 };
